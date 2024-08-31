fdc2437abe42ea6c84cad073d7c70883d361fdf1
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-08-30 22:04:16 -0300

    Created library + small changes

--- app.c
+++ app.c
@@ -1,14 +1,12 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http:\/\/www.viva64.com
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "lib.h"
 
-int main(int argc, char * argv[]){
-    if(argc == 0){
-        fprintf(stderr, "Please input at least one file path");
+int main(int argc, char * argv[]) {
+    if(argc == 0) {
+        fprintf(stderr, "Usage: ./md5 path\n");
         exit(1); 
     }
     exit(0);
-
 }

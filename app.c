a6f4478df6709c6a681d879e05cd881fb042573e
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-08-31 17:41:16 -0300

    Co-authored-by: clee0412 <clee0412@users.noreply.github.com> Co-authored-by: Matías Leporini <LosProgramasdeLepo@users.noreply.github.com> started the function for executing children from app.c

--- app.c
+++ app.c
@@ -4,9 +4,54 @@
 #include "lib.h"
 
 int main(int argc, char * argv[]) {
+    // Check if the amount of arguments is valid
     if(argc == 0) {
         fprintf(stderr, "Usage: ./md5 path\n");
         exit(1); 
     }
     exit(0);
+}
+
+
+
+int make_child_process() {
+    // Create file descriptors for the pipes
+    int appToSlave[2];
+    int slaveToApp[2];
+
+    // Iniciate pipes - check  -1 return -> cant fork :C
+    if(pipe(appToSlave) < 0|| pipe(slaveToApp) < 0) {
+        fprintf(stderr, "Error creating pipe");
+        return ERROR; 
+    
+    // Fork parent process
+    pid_t pid = fork();
+
+    // Invalid process
+    if(pid < 0) {
+        fprintf(stderr, "Error forking parent process");
+        return ERROR;
+    }
+    
+    // Child process 
+    if (pid > 0) {
+        // Close child file descriptors 
+        if (close(appToSlave[WRITE_END]) || close(slaveToApp[READ_END])) {
+            fprintf(stderr, "Error closing pipe ends");
+            return ERROR; 
+        }
+        // excve("./bin/slave", fd) etc etc
+    }
+    
+    // Parent process
+    else {
+        // Close parent file descriptors
+        if (close(appToSlave[READ_END] || close(slaveToApp[WRITE_END]))) {
+            fprintf(stderr, "Error closing pipe ends");
+            return ERROR; 
+        }
+        
+        
+    }
+
 }
--- lib.h
+++ lib.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+
+#define ERROR (-1) 
+
+#define READ_END 0 
+#define WRITE_END 1

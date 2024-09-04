4aa739d3eebc442a15da3f33878bc18e42571dc1
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-09-04 15:43:18 -0300

    slave works fine :D

--- slave.c
+++ slave.c
@@ -6,11 +6,12 @@
 
 
 // slave starts and reads from stdin , and afterwards writes to stdout
-// in mas
+// in master we should change STDIN_FILENO and STDOUT_FILENO 
+// that way we can eexecute slave from master and slave doesnt notice"
 
 int main(){
     // md5 command
-    char command[sizeof("md5sum \"\"") + MAX_FILE_PATH];
+    char command[sizeof("md5sum ")+ 2 /*for quotation marks*/ + MAX_FILE_PATH];
     // our file path to append
     char filePath[MAX_FILE_PATH];
     // where we'll store the
@@ -23,21 +24,32 @@ int main(){
         if(n < 0 ){
             fprintf(stdout, "An error ocurred with file path");
         }
-        // here would go the file validation (is it a dir?),,, optional for late
+
+        // here would go the file validation (is it a dir?),,, optional for later
+
+        // overwrite \n because md5sum doesnt accept it :c
+        if(filePath[n-1] == '\n'){
+            filePath[n-1] = 0;
+        }
+        else{
+            filePath[n] = 0;
+        }
+
         //append the file path as part of the command
-        snprintf(command,sizeof(command),"md5sum \"%s\"", filePath);
+        snprintf(command,sizeof(command),"md5sum \"%s\" ", filePath);
+
         FILE * md5sum = popen(command,"r");
         if(md5sum == NULL){
-            fprintf(stderr, "Could not execute md5sum command: %s", command);
             exit(1);
         }
         if(fgets(result,MAX_HASH,md5sum) == NULL){
-            fprintf(stderr,"An error occured while parsing md5sum result \n");
+            pclose(md5sum);
             exit(1);
         }
         else{
             printf("%s\n",result);
         }
+        pclose(md5sum);        
     }
     exit(0);
 }

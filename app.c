5c0b39f2217af2c0b12e42afb9601ce7c35bb37a
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-09-04 17:53:13 -0300

    Progress in app.c
    Co-authored-by: Ana Negre <anita476@users.noreply.github.com>

--- app.c
+++ app.c
@@ -2,19 +2,25 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http:\/\/www.viva64.com
 
 #include "lib.h"
+#include <unistd.h>
+#include <errno.h>
+#include <sys/select.h>
+
+int make_child_process();
+// create shared memory
+// create fd set for select 
 
 int main(int argc, char * argv[]) {
     // Check if the amount of arguments is valid
-    if(argc == 0) {
+    if(argc < 2) {
         fprintf(stderr, "Usage: ./md5 path\n");
         exit(1); 
     }
+    printf("%d", make_child_process());
     exit(0);
 }
 
-
-
-int make_child_process() {
+int make_child_process(int * readDescriptor, int * writeDescriptor) {
     // Create file descriptors for the pipes
     int appToSlave[2];
     int slaveToApp[2];
@@ -36,22 +42,48 @@ int make_child_process() {
     
     // Child process 
     if (pid == 0) {
-        // Close child file descriptors 
+        // Close child redundant file descriptors 
         if (close(appToSlave[WRITE_END]) || close(slaveToApp[READ_END])) {
             fprintf(stderr, "Error closing pipe ends");
             return ERROR; 
         }
-        // excve("./bin/slave", fd) etc etc
+        
+        // for pipe created "for app" (for reading input data)-> where slave reads, its "stdin" 
+        // for pipe crearted "for slave" (for writing result data) -> where slave writes, its "stdout"
+        if(dup2(appToSlave[READ_END], STDIN_FILENO) < 0 || dup2(slaveToApp[WRITE_END], STDOUT_FILENO) < 0) {
+            fprintf(stderr, "Error duping input and output");
+            return ERROR;
+        }
+        
+        char * const argv[] = {"./slave", NULL};
+        
+        if(execv("./slave", argv)) {
+            fprintf(stderr, "Error in execv");
+            return ERROR;
+        }
     }
-    
+
     // Parent process
     else {
-        // Close parent file descriptors
+        // Close parent redundant file descriptors
         if (close(appToSlave[READ_END] || close(slaveToApp[WRITE_END]))) {
             fprintf(stderr, "Error closing pipe ends");
             return ERROR; 
         }
-        
+
+        //return values:
+        * readDescriptor = slaveToApp[0];
+        * writeDescriptor = appToSlave[1];
+
+        return pid;
     }
 
+    return 0;
+
 }
+
+
+
+
+
+
--- slave.c
+++ slave.c
@@ -9,9 +9,9 @@
 // in master we should change STDIN_FILENO and STDOUT_FILENO 
 // that way we can eexecute slave from master and slave doesnt notice"
 
-int main(){
+int main() {
     // md5 command
-    char command[sizeof("md5sum ")+ 2 /*for quotation marks*/ + MAX_FILE_PATH];
+    char command[sizeof("md5sum ") + 2 /*for quotation marks*/ + MAX_FILE_PATH];
     // our file path to append
     char filePath[MAX_FILE_PATH];
     // where we'll store the
@@ -20,18 +20,18 @@ int main(){
     // we read from "stdin" (file descriptor is a mistery)
     // dont remember why the -1 de PI 
     int n;
-    while((n = read(STDIN_FILENO, filePath, sizeof(filePath) -1))){
-        if(n < 0 ){
+    while((n = read(STDIN_FILENO, filePath, sizeof(filePath) -1))) {
+        if(n < 0) {
             fprintf(stdout, "An error ocurred with file path");
         }
 
         // here would go the file validation (is it a dir?),,, optional for later
 
         // overwrite \n because md5sum doesnt accept it :c
-        if(filePath[n-1] == '\n'){
+        if(filePath[n-1] == '\n') {
             filePath[n-1] = 0;
         }
-        else{
+        else {
             filePath[n] = 0;
         }
 
@@ -39,14 +39,14 @@ int main(){
         snprintf(command,sizeof(command),"md5sum \"%s\" ", filePath);
 
         FILE * md5sum = popen(command,"r");
-        if(md5sum == NULL){
+        if(md5sum == NULL) {
             exit(1);
         }
-        if(fgets(result,MAX_HASH,md5sum) == NULL){
+        if(fgets(result,MAX_HASH,md5sum) == NULL) {
             pclose(md5sum);
             exit(1);
         }
-        else{
+        else {
             printf("%s\n",result);
         }
         pclose(md5sum);        
